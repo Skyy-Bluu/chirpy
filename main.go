ac21a001652e94bcafe3b90a8678bb69ca02eb59
Ahmed Abdalla <bahr@Ahmeds-MacBook-Pro.local>
2026-02-23 15:07:24 +0200

    Add handler for /healthz

--- main.go
+++ main.go
@@ -1,14 +1,21 @@
 package main
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
 
 func main() {
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir("./")))
+	mux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("./"))))
 	mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "OK")
+	})
+
 	httpServer := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
