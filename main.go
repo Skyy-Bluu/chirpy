6d706eeb87a90d3ce8399b3521e61aa991184574
Ahmed Abdalla <bahr@Ahmeds-MacBook-Pro.local>
2026-02-25 11:30:06 +0200

    added a validate chirp endpoint with profanity handling and a http file with test requests

--- main.go
+++ main.go
@@ -1,17 +1,37 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
+	"strings"
 	"sync/atomic"
 )
 
 const plainTextContentType = "text/plain; charset=utf-8"
+const htmlTextContentType = "text/html"
+const applicationJsonContentType = "application/json"
+
+var profaneWords = []string{
+	"kerfuffle", "sharbert", "fornax",
+}
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+}
+
+type chirp struct {
+	Body string `json:"body"`
+}
+
+type errorResponse struct {
+	Value string `json:"error"`
+}
+
+type cleanedChirp struct {
+	CleanedBody string `json:"cleaned_body"`
 }
 
 func (cfg *apiConfig) middlewareIncrementServerHits(next http.Handler) http.Handler {
@@ -23,15 +43,85 @@ func (cfg *apiConfig) middlewareIncrementServerHits(next http.Handler) http.Hand
 }
 
 func (cfg *apiConfig) showMetricsHandler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Add("Content-Type", plainTextContentType)
+	w.Header().Add("Content-Type", htmlTextContentType)
 	w.WriteHeader(http.StatusOK)
-	hitsString := fmt.Sprintf("Hits: %d", cfg.fileserverHits.Load())
+	hitsString := fmt.Sprintf(`
+<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>
+`, cfg.fileserverHits.Load())
 	io.WriteString(w, hitsString)
 }
 
 func (cfg *apiConfig) resetHitsMetricHandler(w http.ResponseWriter, req *http.Request) {
 	w.WriteHeader(http.StatusOK)
 	cfg.fileserverHits.Store(0)
+}
+
+func healthzHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", plainTextContentType)
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, "OK")
+}
+
+func validateChirpHandler(w http.ResponseWriter, req *http.Request) {
+	decoder := json.NewDecoder(req.Body)
+	w.Header().Set("Content-Type", applicationJsonContentType)
+
+	chirp := chirp{}
+	error := errorResponse{}
+	cleanedChirp := cleanedChirp{}
+
+	if err := decoder.Decode(&chirp); err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		error.Value = "Something went wrong"
+		dat, err := json.Marshal(error)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(dat)
+		return
+	}
+
+	if len(chirp.Body) > 140 {
+		w.WriteHeader(http.StatusBadRequest)
+		error.Value = "Chirp is too long"
+		dat, err := json.Marshal(error)
+		if err != nil {
+			log.Printf("Error marshalling JSON: %s", err)
+			w.WriteHeader(http.StatusInternalServerError)
+			return
+		}
+		w.Write(dat)
+		return
+	}
+
+	chirpSlice := strings.Split(chirp.Body, " ")
+
+	for i, word := range chirpSlice {
+		for _, profaneWord := range profaneWords {
+			if strings.ToLower(word) == profaneWord {
+				chirpSlice[i] = "****"
+			}
+		}
+	}
+
+	cleanedString := strings.Join(chirpSlice, " ")
+	w.WriteHeader(http.StatusOK)
+	cleanedChirp.CleanedBody = cleanedString
+	dat, err := json.Marshal(cleanedChirp)
+	if err != nil {
+		log.Printf("Error marshalling JSON: %s", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.Write(dat)
 }
 
 func main() {
@@ -42,13 +132,10 @@ func main() {
 	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir("./")))
 	mux.Handle("/app/", apiConfig.middlewareIncrementServerHits(appHandler))
 	mux.Handle("/assets/logo.png", http.FileServer(http.Dir(".")))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, req *http.Request) {
-		w.Header().Set("Content-Type", plainTextContentType)
-		w.WriteHeader(http.StatusOK)
-		io.WriteString(w, "OK")
-	})
-	mux.HandleFunc("/metrics", apiConfig.showMetricsHandler)
-	mux.HandleFunc("/reset", apiConfig.resetHitsMetricHandler)
+	mux.HandleFunc("GET /api/healthz", healthzHandler)
+	mux.HandleFunc("GET /admin/metrics", apiConfig.showMetricsHandler)
+	mux.HandleFunc("POST /admin/reset", apiConfig.resetHitsMetricHandler)
+	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 
 	httpServer := http.Server{
 		Handler: mux,
@@ -59,4 +146,13 @@ func main() {
 		log.Fatal(err.Error())
 	}
 
+}
+
+func containsAnyInSlice(s string, slice []string) bool {
+	for _, word := range slice {
+		if strings.Contains(s, word) {
+			return true
+		}
+	}
+	return false
 }
